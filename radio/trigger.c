ca3c638173d1a333e599ae738643f48ee95d7013
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2014-08-15 19:40:06 -0400

    Switched names as it was quite confusing.

--- radio/trigger.c
+++ radio/trigger.c
@@ -1,86 +1,112 @@
+// Connects to camera
+
 #include <stdlib.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
 #define F_CPU 1000000UL  // 1 MHz
 #include <util/delay.h>
 
 #include "rf.h"
 
-#define PIN_BUTTON_FOCUS PCINT1
-#define PIN_BUTTON_PHOTO PCINT0
-#define PIN_BUTTON_LASER PCINT2
+#define PIN_PHOTO PB0
+#define PIN_FOCUS PB1
+
 #define PIN_LIGHT PB4
 
-void send_bit(uint8_t bit) {
-  PORTB &= ~(1<<PIN_COMM);
-  _delay_ms(PULSE);
-  PORTB |= 1<<PIN_COMM;
-  if (bit) {
-    _delay_ms(SHORT);
-  }
-  else {
-    _delay_ms(LONG);
-  }
-  PORTB &= ~(1<<PIN_COMM);
-  _delay_ms(PULSE);
-}
+#define STATE_WAITING 0
+#define STATE_INIT 1
+#define STATE_READY 2
+#define STATE_RECV 3
 
-void send(uint8_t byte) {
-  uint8_t i;
-  for (i=0; i<8; i++) {
-    send_bit(byte&1);
-    byte >>=1;
-  }
-}
+#define RCV_SHORT 1
+#define RCV_LONG 2
 
-//@TODO interrupt.
-ISR(PCINT0_vect) {
-  // ~PINB to get falling edge since using pull-up.
-  uint8_t status = ~PINB;
-  PORTB |= 1<<PIN_LIGHT;
-  if (status & (1<<PIN_BUTTON_PHOTO)) {
-    send(MESSAGE_PHOTO);
-    PORTB ^= 1<<PIN_LIGHT;
-  }
-  // Trigger on rising edge since photo transistor on by default.
-  // @TODO Fix this, triggers on release of other buttons now.
-  // @solution: detect if it's low at start time and setup only in that situation.
-  /* else if (~status & (1<<PIN_BUTTON_LASER)) */
-  /*   send(MESSAGE_PHOTO); */
-  else if (status & (1<<PIN_BUTTON_FOCUS)) {
-    send(MESSAGE_FOCUS);
-    // Check if released during message transmission
-    uint8_t status = PINB;
-    if (status & (1<<PIN_BUTTON_FOCUS)) {
-      send(MESSAGE_RELEASE);
+//@TODO timed release using interrupts
+
+uint8_t count() {
+  uint8_t count = 0;
+  uint8_t state = 0; 
+  while (1) {
+    state = PINB & (1<<PIN_COMM);
+    if (state) {
+      count++;
+      count &= 15U;
     }
+    else {
+      if (count) {
+        return count;
+      }
+    }
+    _delay_us(SAMPLE);
   }
-  // Release
-  else {
-    send(MESSAGE_RELEASE);
+}
+
+uint8_t interpret(uint8_t count) {
+  if (count < 3) return 0;
+  if (count < 7) return RCV_SHORT;
+  if (count < 10) return RCV_LONG;
+}
+
+void photo() {
+  // Set pin to output (ground it)
+  // Set focus pin too to make sure camera is awake (focus pin is live
+  // during sleep)
+  DDRB |= (1<<PIN_FOCUS|1<<PIN_PHOTO);
+  PORTB |= 1<<PIN_LIGHT;
+  _delay_ms(10);
+  DDRB &= ~(1<<PIN_FOCUS);
+  _delay_ms(90);
+  DDRB &= ~(1<<PIN_PHOTO);
+  PORTB &= ~(1<<PIN_LIGHT|1<<PIN_PHOTO);
+}
+
+void focus() {
+  DDRB |= (1<<PIN_FOCUS);
+}
+
+void release() {
+  DDRB &= ~(1<<PIN_FOCUS|1<<PIN_PHOTO);
+}
+
+
+/**
+ * Analyze latest received sample.
+ */
+void listen() {
+  uint8_t bit, bits, byte;
+  bit = bits = byte = 0;
+  while (1) {
+    bit = interpret(count());
+    if (!bit) {
+      byte = bits = 0;
+    }
+    else {
+      if (bit == RCV_SHORT) {
+        byte |= 1<<bits;
+      }
+      bits++;
+    }
+    if (bits == 8) {
+      if (byte == MESSAGE_PHOTO) {
+        photo();
+      }
+      else if (byte == MESSAGE_FOCUS) {
+        focus();
+      }
+      else {
+        release();
+      }
+    }
   }
 }
 
 
 void main() {
-  // Let time for voltage to stabilize
-  DDRB |= 1<<PIN_COMM | 1<<PIN_LIGHT;
+  // Pins set to input (high resistance)
+  _delay_ms(100);
+  DDRB |= 1<<PIN_LIGHT;
   PORTB |= 1<<PIN_LIGHT;
-  _delay_ms(500);
+  _delay_ms(900);
   PORTB ^= 1<<PIN_LIGHT;
-  // Set pull-up
-  PORTB |= (1<<PIN_BUTTON_FOCUS)|(1<<PIN_BUTTON_PHOTO);
-  // Setup interrupts for button presses.
-  PCMSK |= (1<<PIN_BUTTON_FOCUS)|(1<<PIN_BUTTON_PHOTO);
-  GIMSK |= (1<<PCIE);
-  sei();
-  while (1) {
-    // Toggle pin every PULSE ms to keep active carrier signal.
-    PORTB |= 1<<PIN_COMM;
-    _delay_ms(PULSE);
-    PORTB &= ~(1<<PIN_COMM|1<<PIN_LIGHT);
-    _delay_ms(30);
-    send(MESSAGE_RELEASE);
-  }
+  listen();  
 }
